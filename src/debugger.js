8a8a593a668672ba5e91b9e47f4ada631746679c
Jason Laster <jason.laster.11@gmail.com>
2017-11-01 12:22:23 -0400

    small updates

--- src/debugger.js
+++ src/debugger.js
@@ -1,5 +1,6 @@
 const chalk = require("chalk");
-
+// const hankey = require("hankey");
+// var emoji = require("node-emoji");
 function pausedAction (action) {
   const why = action.pauseInfo.why.type;
   const frame = action.frames[0];
@@ -16,6 +17,22 @@ function formatSource (source) {
   return `${source.id} - ${formatUrl(source.url)}`;
 }
 
+let counts = {};
+
+const map = {
+  ADD_SOURCE: d => formatSource(d.source),
+  CONNECT: d => `${d.url}`,
+  PAUSED: d => pausedAction(d),
+  SELECT_SOURCE: d => formatSource(d.source),
+  LOAD_SOURCE_TEXT: d => formatSource(d.source),
+  EVALUATE_EXPRESSION: d => `${d.input}`,
+  COMMAND: d => `${d.command}`
+};
+
+// const emojis = {
+  // PAUSED: "clock130"
+// };
+
 function onAction (line, data) {
   const [, action, status, actionData] = line.match(
     /\[ACTION\] (\w+)\s*(\[\w+\])?\s*-\s*(.*)$/
@@ -31,18 +48,23 @@ function onAction (line, data) {
     console.log(`${action} FAILED`);
   }
 
-  const map = {
-    ADD_SOURCE: d => formatSource(d.source),
-    CONNECT: d => `${d.url}`,
-    PAUSED: d => pausedAction(d),
-    SELECT_SOURCE: d => formatSource(d.source),
-    LOAD_SOURCE_TEXT: d => formatSource(d.source),
-    COMMAND: d => `${d.value.type}`
-  };
+  if (action in counts) {
+    if (!status || status.includes("start")) {
+      counts[action] = counts[action] + 1;
+    }
+  } else {
+    counts[action] = 1;
+  }
 
   let dataStr = action in map ? map[action](dataObj) : "";
-  const statusStr = status ? `${status} ` : "";
-  return chalk`   {blue.dim ${action}} {dim ${statusStr}${dataStr} }`;
+  const statusStr = !status
+    ? ""
+    : status.includes("start")
+      ? chalk`{green ${status}} `
+      : chalk`{red ${status}} `;
+  const count = counts[action];
+  const icon = ""; // action in emojis ? `${emoji.get(emojis[action])} ` : "";
+  return chalk`   {dim {blue ${count} ${action}} ${icon}${statusStr}${dataStr}}`;
 }
 
 function onGecko (line, data) {
