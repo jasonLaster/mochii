3b1c5995d5f060c5423a8e2bd52170ca64be5a6d
Jason Laster <jason.laster.11@gmail.com>
2017-11-07 15:17:57 -0500

    switch to cases

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,65 +1,30 @@
 const readFile = require("./helpers/readFile");
 const { readOutput } = require("../index");
+const cases = require("jest-in-case");
 
 function simulateMochitest(file) {
-  const out = readOutput(readFile(file));
+  const out = readOutput(readFile(file), { ci: false });
   // console.log(out.join("\n"));
   return out;
 }
 
-describe("mochi", () => {
-  it("basic", () => {
-    const out = simulateMochitest("basic.txt");
+cases(
+  "mochi",
+  ({ name }) => {
+    const out = simulateMochitest(name);
     expect(out).toMatchSnapshot();
-  });
-
-  it("full run", () => {
-    const out = simulateMochitest("full.txt");
-    expect(out).toMatchSnapshot();
-  });
-
-  it("error", () => {
-    const out = simulateMochitest("error.txt");
-    expect(out).toMatchSnapshot();
-  });
-
-  it("ufail", () => {
-    const out = simulateMochitest("ufail.txt");
-    expect(out).toMatchSnapshot();
-  });
-
-  it("mochi-fail", () => {
-    const out = simulateMochitest("mochi-fail.txt");
-    expect(out).toMatchSnapshot();
-  });
-
-  it("build-fail", () => {
-    const out = simulateMochitest("build-fail.txt");
-    expect(out).toMatchSnapshot();
-  });
-
-  it("console_error", () => {
-    const out = simulateMochitest("console_error.txt");
-    expect(out).toMatchSnapshot();
-  });
-
-  it("actions", () => {
-    const out = simulateMochitest("actions.txt");
-    expect(out).toMatchSnapshot();
-  });
-
-  it("frame", () => {
-    const out = simulateMochitest("frame.txt");
-    expect(out).toMatchSnapshot();
-  });
-
-  it("error2", () => {
-    const out = simulateMochitest("error2.txt");
-    expect(out).toMatchSnapshot();
-  });
-
-  it("unformatted stack", () => {
-    const out = simulateMochitest("unformatted_stack.txt");
-    expect(out).toMatchSnapshot();
-  });
-});
+  },
+  [
+    { name: "basic.txt" },
+    { name: "full.txt" },
+    { name: "error.txt" },
+    { name: "ufail.txt" },
+    { name: "mochi-fail.txt" },
+    { name: "build-fail.txt" },
+    { name: "console_error.txt" },
+    { name: "actions.txt" },
+    { name: "frame.txt" },
+    { name: "error2.txt" },
+    { name: "unformatted_stack.txt" }
+  ]
+);
